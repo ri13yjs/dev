4b9d627388e1d186f476423babdad4abdee4fed0
ri13yjs <rjs9405@nyu.edu>
2026-03-30 15:07:02 +0200

    added sumOfPowers

---
+++ CSOAssignment3/sumOfPowers.s
@@ -0,0 +1,31 @@
+.global sumOfPowers
+
+sumOfPowers: 
+    push %rbp
+    mov %rsp, %rbp
+
+    mov %rdi, %rbx
+    mov $0, %rax
+    move $1, %rcx
+
+Top:
+    #comparison input, iterator
+    cmp %rbx, %rcx
+    jg Done
+
+    mov %rcx, %rdx
+    imul %rdx, %rdx
+    add %rdx, %rax
+
+    inc %rcx
+    jmp Top
+
+Done:
+    pop %rbp
+    ret
+    
+
+
+
+
+
